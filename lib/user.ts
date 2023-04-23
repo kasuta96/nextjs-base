188f823edb54f3faa5fede191f332a54cd397983
ThangCx <thangcx@nal.vn>
2023-04-23 19:14:38 +0900

    Add homepage & logout page

--- lib/user.ts
+++ lib/user.ts
@@ -1,12 +1,17 @@
 import { redirect } from 'next/navigation'
 import { getCurrentUser } from './session'
 
-export async function CheckUser() {
+export async function CheckUser(callbackUrl?: string) {
   const user = await getCurrentUser()
-  if (!user) return redirect('/login')
+  if (!user) {
+    return redirect(
+      `/logout` +
+        (callbackUrl ? `?from=${encodeURIComponent(callbackUrl)}` : '')
+    )
+  }
 
   if (['Inactive', 'Pending', 'Blocked'].includes(user.status))
-    redirect('error/403')
+    return redirect('/error/403')
 
   return user
 }
