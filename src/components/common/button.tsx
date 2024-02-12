ad1ea599bf80308e5cff0f7e59813526724e7ecf
kasuta96 <kasuta96@gmail.com>
2024-02-12 22:39:27 +0900

    Update prisma & button in users page

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -1,25 +1,38 @@
-import { Button } from "@/components/ui/button"
+import { Button, ButtonProps } from "@/components/ui/button"
 import { Loader2, SaveIcon } from "lucide-react"
 import { useTranslations } from "next-intl"
 
-export default function SaveButton({
+export function SaveButton({
   loading,
-  disabled,
   ...props
-}: {
+}: ButtonProps & {
   loading?: boolean
-  disabled?: boolean
 }) {
   const t = useTranslations()
 
   return (
-    <Button type="submit" disabled={disabled} {...props}>
+    <Button type="submit" {...props}>
       {loading ? (
         <Loader2 className="mr-2 h-4 w-4 animate-spin" />
       ) : (
         <SaveIcon className="mr-2 h-4 w-4" />
       )}
       {t("common.Save")}
+    </Button>
+  )
+}
+
+export function LoadingButton({
+  loading,
+  children,
+  ...props
+}: ButtonProps & {
+  loading?: boolean
+  children?: React.ReactNode
+}) {
+  return (
+    <Button type="submit" disabled={loading} {...props}>
+      {loading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : children}
     </Button>
   )
 }
